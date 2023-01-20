9db61ab9a37fefcb6546a3acf430528ea44d2e52
Randall <randall.etzweiler@gmail.com>
2023-01-20 18:09:29 +0100

    modification fonctions

--- src/analysis/importerPython.py
+++ src/analysis/importerPython.py
@@ -1,43 +1,49 @@
+from typing import List
 from lxml import etree
 import json
 import os
-import ParkingData
+from ParkingData import ParkingData
 from matplotlib import pyplot as plt 
 import time
-import datetime
+from datetime import datetime
 
 
-def car():
+def car() -> List[ParkingData]:
     parkings = []
-    for sample in os.listdir("./data"):
-        for xml in os.listdir("./data"+sample):
-            etree.parse(xml)
-            date = etree.xpath("/park/DateTime")
-            name = etree.xpath("/park/Name")
-            status=etree.xpath["/park/Status"],
-            total = etree.xpath("/park/Total")
-            free = etree.xpath("/park/Name")
-            monParking = ParkingData(date=date, name=name, status=status, total=total, free=free)
-            parkings.append(monParking)
+    for sample in os.listdir("./data/carParks"):
+        for xml in os.listdir("./data/carParks/"+sample):
+            try:
+                tree = etree.parse(f"./data/carParks/{sample}/{xml}")
+                date = int(datetime.fromisoformat(tree.xpath("/park/DateTime")[0].text.split('.')[0]).timestamp())
+                name = tree.xpath("/park/Name")[0].text
+                status = tree.xpath("/park/Status")[0].text
+                total = int(tree.xpath("/park/Total")[0].text)
+                free = int(tree.xpath("/park/Free")[0].text)
+
+                monParking: ParkingData = ParkingData(date=date, name=name, status=status, total=total, free=free)
+                parkings.append(monParking)
+            except etree.XMLSyntaxError:
+                #print(f"Could not parse {xml}: {e}")
+                pass
     return parkings
     
 def bicycle():
+
         parkings = []
-        for sample in os.listdir("./data"):
-            with open("../data/carParks/"+sample+"/data.json", 'r', encoding='utf8') as data_file:
-                data_json = json.parse(data_file);
-                for station in data_json["data"]["station"]:
+        for sample in os.listdir("data/bicyclePark"):
+            with open("data/bicyclePark/"+sample+"/data.json", 'r', encoding='utf8') as data_file:
+                data_json = json.JSONDecoder().decode("\n".join(data_file.readlines()))
+                for station in data_json["data"]["stations"]:
+                    print(station["last_reported"], station["station_id"],station["is_installed"],int(station["num_docks_available"]), int(station["num_bikes_available"]) )
                     parking = ParkingData(
                         date=station["last_reported"],
                         name=station["station_id"],
                         status=station["is_installed"],
-                        total=station["bike_total"],
-                        free=station["bike_available"]
+                        total= int(station["num_docks_available"]),
+                        free=int(station["num_bikes_available"])
                     )
-                    parkings.append(parking)
+                    parkings.append(parking) 
         return parkings
-
-print(os.listdir("../../"))
 
 parkings = bicycle.__annotations__
 #for parking in parkings...
@@ -88,23 +94,22 @@ def grafic_bicycle(date):
     plt.show()"""
 
 
-def grafic_parkings(vehicule, date):
-    parkings=vehicule()
+def plot_parkings(parkingsdata, date: int):
     x=[]
     y1=[]
     y2=[]
-    parking_date=[p for p in parkings if p.getDate()==date]
+    parking_date=[p for p in parkingsdata if (p.getDate() < date)]
     for parking in parking_date:
         x.append(parking.getName())
-        y1.append(parking.geTotal())
-        y2.append(parking.geTotal())-parking.getFree()/(parking.geTotal())
-    plt.bar(x, y1)
+        y1.append(parking.getTotal())
+        y2.append((parking.getFree()/(parking.getTotal())*100))
+    #plt.bar(x, y1)
     plt.bar(x, y2)
     plt.show()
 
 #main
-date=datetime.datetime(2023,1,16,19,14,59)
-grafic_parkings(car, date)
-#grafic_parkings(bicycle,date)
+date=int(datetime(2023, 1, 1).timestamp())
+#plot_parkings(car(), date)
+plot_parkings(bicycle(),date)
 
 
